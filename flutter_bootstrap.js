c4ca66911cf7db9c8a14fcf26562d537bfc4702d
leonlau4148 <ravenpauladrales@gmail.com>
2026-06-11 18:04:41 +0800

    Rebuild with correct base-href for Cloudflare

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2795205938"
+    serviceWorkerVersion: "1701562410"
   }
 });
